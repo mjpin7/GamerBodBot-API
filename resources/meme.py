78434730ea0950c4e6eb07d1f3f49502b346cbe5
MJ Pinheiro <mjpin7@gmail.com>
2019-04-13 19:41:20 -0400

    Commited a better way to return response to bot

--- resources/meme.py
+++ resources/meme.py
@@ -1,5 +1,4 @@
 from flask_restful import Resource
-from flask import make_response, Response
 from flask_jwt_extended import (
     jwt_required,
     create_access_token, 
@@ -13,6 +12,5 @@ class Meme(Resource):
     @jwt_required
     def get(self):
         rand_num = randint(1, 429)
-        response = make_response("https://github.com/mjpin7/GamerBodBot-API/tree/master/files/pics{}.jpg".format(rand_num), 200)
-        response.headers.extend({'Content-Type': 'image/jpg'})
-        return response
+
+        return {'Meme': "https://raw.githubusercontent.com/mjpin7/GamerBodBot-API/master/files/pics/{}.jpg".format(rand_num)}
