54458e653eaceb067afedeaf51c66a416b3a065f
MJ Pinheiro <mjpin7@gmail.com>
2019-04-13 19:34:06 -0400

    Testing fix for response

--- resources/meme.py
+++ resources/meme.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from flask import make_response, Response
 from flask_jwt_extended import (
     jwt_required,
     create_access_token, 
@@ -12,5 +13,6 @@ class Meme(Resource):
     @jwt_required
     def get(self):
         rand_num = randint(1, 429)
-
-        return {'meme': "files/pics/{}.jpg".format(rand_num)}, 200, {'Content-Type': 'image/jpeg'}
+        response = make_response({'meme': "https://github.com/mjpin7/GamerBodBot-API/tree/master/files/pics{}.jpg".format(rand_num)}, 200)
+        response.headers.extend({'Content-Type': 'image/jpeg'})
+        return response
