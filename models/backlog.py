c5da62642b7cc5595d3d7ba2eb8a01e74fd1437b
MJ Pinheiro <mjpin7@gmail.com>
2019-04-21 12:22:21 -0400

    Renamed column for Backlog

--- models/backlog.py
+++ models/backlog.py
@@ -1,18 +1,18 @@
 from db import db
 
 class BacklogItemModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     game = db.Column(db.String(80))
     status = db.Column(db.String(20))
 
     def __init__(self, _id, game, status):
-        self.id = _id
+        self.user_id = _id
         self.game = game
         self.status = status
 
     def json(self):
         return{
-            'id': self.id,
+            'user_id': self.user_id,
             'game': self.game,
             'status': self.status
         }
@@ -27,9 +27,9 @@ class BacklogItemModel(db.Model):
 
     @classmethod
     def find_all_by_id(cls, _id):
-        return cls.query.filter_by(id=_id).all()
+        return cls.query.filter_by(user_id=_id).all()
 
     @classmethod
     def find_by_game(cls, _id, _game):
-        return cls.query.filter_by(id=_id, game=_game).first()
+        return cls.query.filter_by(user_id=_id, game=_game).first()
     
