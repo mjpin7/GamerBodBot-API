4ee84a278c7beca766c530bd4fcd8246c322e471
MJ Pinheiro <mjpin7@gmail.com>
2019-04-21 12:15:30 -0400

    Added missing dependancy

--- resources/backlog.py
+++ resources/backlog.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, reqparse
 from models.backlog import BacklogItemModel
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
+    jwt_required,
     create_access_token, 
     create_refresh_token, 
     jwt_refresh_token_required,
